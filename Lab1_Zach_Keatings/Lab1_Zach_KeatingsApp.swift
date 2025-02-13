9ab230278c788f97969309f6663cd3ab17e1f1d7
Zach Keatings <zachary.keatings@gmail.com>
2025-02-13 11:17:31 -0500

    Initial Commit

---
+++ Lab1_Zach_Keatings/Lab1_Zach_KeatingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Zach_KeatingsApp.swift
+//  Lab1_Zach_Keatings
+//
+//  Created by Zach Keatings on 2025-02-13.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Zach_KeatingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
